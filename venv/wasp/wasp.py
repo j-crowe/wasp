221f6d5919e6ef2441d38912008856520f2e242a
Jeremy Crowe <crowe.jb@gmail.com>
2016-03-10 12:23:12 -0800

    new hatching function and better err handling

--- venv/wasp/wasp.py
+++ venv/wasp/wasp.py
@@ -9,22 +9,26 @@ def parse_torrent():
     torrent_file = raw_input("Torrent File Location: ")
 
     try:
+        # Attempt to open the torrent file and bdecode the metadata
         with open(torrent_file, 'r') as content_file:
             t_content = content_file.read()
-            meta_data = bencode.bdecode(t_content)
-            print meta_data
-            hatchling = Wasp(meta_data)
-
+            hatch(bencode.bdecode(t_content))
+    except IOError:
+        print 'ERROR: Could not open file: ' + torrent_file
     except:
-        print 'WASP: Error opening file'
+        print 'ERROR: An unknown error occurred in opening or bdecoding the file.'
 
+
+def hatch(meta_data):
+    "DESC: generate a new wasp"
+    hatchling = Wasp(meta_data)
 
 
 class Wasp(object):
     "DESC: A wasp is a single instance of a torrent. Includes all metadata and functionality in seeding/leeching"
 
     def __init__(self, meta_data):
-        import pdb; pdb.set_trace()
+        # TODO: Handle non-existant vars appropriately
         self.announce = meta_data['announce']
         self.piece_length = meta_data['info']['piece length']
         self.pieces = meta_data['info']['pieces']
