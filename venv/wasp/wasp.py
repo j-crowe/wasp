8833ea7d79e81bee824120814930c9d5def48db7
Jeremy Crowe <crowe.jb@gmail.com>
2016-03-10 12:11:23 -0800

    handle individual file. Need to fix this for multi file torrents though

--- venv/wasp/wasp.py
+++ venv/wasp/wasp.py
@@ -4,7 +4,7 @@ def main():
     parse_torrent()
 
 def parse_torrent():
-    '''DESC: Asks user for location of .torrent file. Parses and saves metadata results'''
+    "DESC: Asks user for location of .torrent file. Parses and saves metadata results"
     t_content = ''
     torrent_file = raw_input("Torrent File Location: ")
 
@@ -13,21 +13,25 @@ def parse_torrent():
             t_content = content_file.read()
             meta_data = bencode.bdecode(t_content)
             print meta_data
-            import pdb; pdb.set_trace()
+            hatchling = Wasp(meta_data)
+
     except:
         print 'WASP: Error opening file'
 
 
 
 class Wasp(object):
+    "DESC: A wasp is a single instance of a torrent. Includes all metadata and functionality in seeding/leeching"
 
     def __init__(self, meta_data):
+        import pdb; pdb.set_trace()
         self.announce = meta_data['announce']
         self.piece_length = meta_data['info']['piece length']
         self.pieces = meta_data['info']['pieces']
-        # TODO: Only works on single file torrents
         self.name = meta_data['info']['name']
-        self.length = meta_data['info']['length']
+        # TODO: Only works on single file torrents
+        file = meta_data['info']['files'][0]
+        self.length = file['length']
 
     def apple(self):
         print "I AM CLASSY APPLES!"
