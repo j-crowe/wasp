a8bd193d0558648518c9f9357971d0f6f8e59886
Jeremy Crowe <crowe.jb@gmail.com>
2016-03-14 17:06:19 -0700

    appropriate location of nest object

--- venv/wasp/nest.py
+++ venv/wasp/nest.py
@@ -1,1 +1,27 @@
 import wasp
+
+
+class Nest(object):
+
+    def __init__(self):
+        self.colony = {}
+
+    def hatch(self, meta_data):
+        "DESC: generate a new wasp"
+        hatchling = wasp.Wasp(meta_data)
+        self.assimilate(hatchling)
+        hatchling.get_peers()
+
+    def assimilate(self, hatchling):
+        # TODO: check if torrent already exists in colony
+        if hatchling.info_hash in self.colony:
+            print "ALERT: torrent file already exists"
+        self.colony[hatchling.info_hash] = hatchling
+
+    def destroy(self, wasp):
+        # TODO: Check if was exists, destroy it and all saved and associated data
+        print "destroy"
+
+    def swarm(self):
+        # TODO: Activate the swarm. Spin off threads for each wasp in colony to seed or leech
+        print "swarm"
