e324abc609c2294fd39dd1ab65d19c1e9a566692
Jeremy Crowe <crowe.jb@gmail.com>
2017-01-27 16:19:07 -0800

    reformat files to be pep8 compatible

--- wasp/utils.py
+++ wasp/utils.py
@@ -6,9 +6,10 @@ from struct import pack, unpack
 
 
 def decode_port(port):
-    "DESC: Given big endian encoded port, returns numerical port."
+    """DESC: Given big endian encoded port, returns numerical port."""
 
     return unpack(">H", port)[0]
+
 
 def decode_expanded_peers(peers):
     """ Return a list of IPs and ports, given an expanded list of peers,
@@ -16,14 +17,17 @@ def decode_expanded_peers(peers):
 
     return [(p["ip"], p["port"]) for p in peers]
 
+
 def decode_binary_peers(peers):
-    "DESC: Parse out binary peers list and return list of IPs and ports for connection attempts"
+    """DESC: Parse out binary peers list and
+    return list of IPs and ports for connection attempts"""
     peers = [peers[i:i+6] for i in range(0, len(peers), 6)]
     return [(socket.inet_ntoa(p[:4]), decode_port(p[4:])) for p in peers]
 
 
 def parse_peerlist(response):
-    "DESC: Given a response, parse out peers list and call appropriate decoding function for either protocol standard encoding."
+    """DESC: Given a response, parse out peers list and call appropriate
+    decoding function for either protocol standard encoding."""
     peer_dict = bencode.bdecode(response.content)
     peers = peer_dict['peers']
     if type(peers) == str:
@@ -31,5 +35,7 @@ def parse_peerlist(response):
     elif type(peers) == list:
         return decode_expanded_peers(peers)
 
+
 def socket_id(sock):
-    return hashlib.md5(sock.getpeername()[0] + str(sock.getpeername()[1])).hexdigest()
+    return hashlib.md5(sock.getpeername()[0] +
+                       str(sock.getpeername()[1])).hexdigest()
